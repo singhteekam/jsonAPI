c32ae6e39b511f766f507e9f4bee02bc246b06a3
Teekam Singh <teekams10999@gmail.com>
2023-05-08 13:43:22 +0530

    added fruits file

--- app.js
+++ app.js
@@ -3,22 +3,15 @@ const app= express();
 const port= process.env.PORT || 5000;
 
 const quiz= require("./quiz.json");
+const fruits= require("./fruits.json");
 
 app.get("/quiz", (req, res)=>{
     res.send(quiz);
 });
 
-app.get("/parse", (req, res)=>{
-    const url = "https://app.singhteekam.in/QuizAPI-Node.js/data.json";
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((jsonData) =>{
-         console.log(jsonData)
-         res.send(jsonData);
-      }
-         );
-});
+app.get("/fruits", (req, res)=>{
+    res.send(fruits);
+}); 
 
 
 
